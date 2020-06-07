9e291efe4b740f4cc0bb9453e8bcf17f59306d9e
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-06-07 20:01:47 +0900

    リクエスト完了ページ改修

--- resources/views/student/store.blade.php
+++ resources/views/student/store.blade.php
@@ -1,1 +1,22 @@
-リクエスト完了です
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">リクエスト完了です</div>
+
+                <div class="card-body">
+                  <form method="GET" action="{{route('userinfo.index')}}">
+                    <button type="submit" class="btn btn-primary">
+                        カード一覧に戻る
+                    </button>
+                  </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
