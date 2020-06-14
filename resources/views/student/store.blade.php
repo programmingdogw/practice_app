913cc57a12d49e72ced53c96420ece49e90bea0b
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-06-14 16:04:01 +0900

    とりあえず応急処置

--- resources/views/student/store.blade.php
+++ resources/views/student/store.blade.php
@@ -8,6 +8,8 @@
                 <div class="card-header">リクエスト完了です</div>
 
                 <div class="card-body">
+                  <p>リクエストは再度送れますが、連投するのはやめましょう。</p>
+                  <p>ユーザー詳細から送ったリクエストが確認できます。</p>
                   <form method="GET" action="{{route('userinfo.index')}}">
                     <button type="submit" class="btn btn-primary">
                         カード一覧に戻る
